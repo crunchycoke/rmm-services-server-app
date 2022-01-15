4002d6aee6ac0075b942da16528d531069f212fd
crunchycoke <patrickjc1995@gmail.com>
2022-01-14 23:52:51 -0500

    Updated Customer Device specific class code with getters for all devices, all customer specific devices, and a specific customer device. Added create, update, and delete support for customer specific devices. Added new queries for getting and deleting customer devices.

--- src/main/java/com/ninjaone/serverapp/repository/CustomerDeviceRepository.java
+++ src/main/java/com/ninjaone/serverapp/repository/CustomerDeviceRepository.java
@@ -2,6 +2,35 @@ package com.ninjaone.serverapp.repository;
 
 import com.ninjaone.serverapp.models.CustomerDevice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 public interface CustomerDeviceRepository extends JpaRepository<CustomerDevice, Long> {
+
+    // Get all devices from customer
+    @Query("select device " +
+            "from CustomerDevice device " +
+            "where device.customer.id = :customerId")
+    List<CustomerDevice> getCustomerDevicesByCustomerId(@Param("customerId") Long customerId);
+
+    // Get specific device from customer
+    @Query("select device " +
+            "from CustomerDevice device " +
+            "where device.id = :id and device.customer.id = :customerId")
+    Optional<CustomerDevice> getCustomerDeviceById(@Param("id") Long id,
+                                                   @Param("customerId") Long customerId);
+
+    // Delete device tied to specific customer
+    @Transactional
+    @Modifying
+    @Query("delete from CustomerDevice device " +
+            "where device.id = :id " +
+            "and device.customer.id = :customerId")
+    int deleteByDeviceId(@Param("id") Long id,
+                         @Param("customerId") Long customerId);
 }
